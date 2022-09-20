e7d11e726ce3ab3e06a17cd43e871b980e902f24
bil_al34 <pet21051@byui.edu>
2022-09-20 13:46:25 -0600

    checkpoint 02 but in c#

--- hello_world_alex/Program.cs
+++ hello_world_alex/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+//File: check02_instructor.py
+//Author: Brother Burton
+
+//Purpose: Display text to the screen.
+
+
+Console.WriteLine("Enter yor first name: ");
+string? first = Console.ReadLine();
+
+Console.WriteLine("Enter your last name: ");
+string? last = Console.ReadLine();
+
+Console.WriteLine($"Your name is {last}, {first} {last}");
+
