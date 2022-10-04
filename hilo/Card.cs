362a1b63137e00b169e6292fc99193b6f10390f9
bil_al34 <pet21051@byui.edu>
2022-10-04 13:45:16 -0600

    updates to Hilo

---
+++ hilo/Card.cs
@@ -0,0 +1,9 @@
+// Class representing the cards in the deck
+class Card {
+    
+    string card;
+
+    public Card(string number) {
+        card = number;
+    }
+}
--- hilo/Hilo.cs
+++ hilo/Hilo.cs
@@ -1,2 +1,34 @@
 ﻿class Hilo {
+// Program will run from here.
+ static void Main(string[] args) {
+
+    Deck deck = new Deck();
+    
+    Card card = deck.DrawCard();
+
+    int score = 300;
+
+    Console.WriteLine($"the card is: {card}");
+
+    Console.Write("Higher or Lower? (h/l): ");
+    string? playerGuess = Console.ReadLine();
+    
+    score = GetPoints(score, isRight);
+    Console.WriteLine($"Your score is: {score}");
+    
+    Console.Write("Play Again? (y/n): ");
+    string? playAgain = Console.ReadLine();
+    
+ } 
+
+static bool GuessRight(string playerGuess, string card) {
+    return true;
+}
+static int GetPoints(int currentPoints, bool isRight) {
+    currentPoints += 100;
+    return 2;
+}
+
+
+
 }
