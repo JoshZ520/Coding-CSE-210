d2dd5f3b32e0cb49557b5468f4cf06e53ac13197
JoshZ520 <joshuazob@gmail.com>
2022-10-06 14:58:05 -0600

    idk

---
+++ hello_world_z/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("What is your first name? ");
+string? first = Console.ReadLine();
+Console.Write("What is your last name? ");
+string? last = Console.ReadLine();
+Console.WriteLine($"My name is {last}, {first} {last}");
+
+
