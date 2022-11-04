37e6eb09c88ba64c8fd8077bcecd4f0855235fe5
JoshZ520 <joshuazob@gmail.com>
2022-11-04 12:25:47 -0600

    Update

--- Greed/MovingObj.cs
+++ Greed/MovingObj.cs
@@ -1,9 +1,5 @@
 class MovingObj {
 
-
-    virtual public void Draw() {
-        // Base game objects do not have anything to draw
-    }
     public int Size { get; set; }
     public MovingObj(int size) {
 
