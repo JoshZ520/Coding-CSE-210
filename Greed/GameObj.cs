b8286c9add0381e8365d317d13d4363a4e8f4362
bil_al34 <pet21051@byui.edu>
2022-11-05 13:58:24 -0600

    getStartPos added to player

--- Greed/GameObj.cs
+++ Greed/GameObj.cs
@@ -3,6 +3,15 @@ using Raylib_cs;
 
 class Player: MovingObj {
     int size = 5;
+
+    public List<float> GetStartPos(int ScreenWidth) {
+        float x_pos = ScreenWidth / 2;
+        float y_pos = 5;
+
+        List<float> startPoint = new List<float> {x_pos, y_pos};
+
+        return startPoint;
+    }
 }
 
 class Gems: MovingObj {
