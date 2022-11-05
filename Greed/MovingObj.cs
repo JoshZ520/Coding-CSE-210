174c6884fb348f73d6bc532f6af562456de6fba9
bil_al34 <pet21051@byui.edu>
2022-11-05 13:38:36 -0600

    update movingobj and gameobj

--- Greed/MovingObj.cs
+++ Greed/MovingObj.cs
@@ -1,8 +1,24 @@
 class MovingObj {
     
     public int Size { get; set; }
-    public MovingObj(int size = 1) {
+    public int Speed { get; set; }
+    public MovingObj(int size = 1, int speed = 1) {
 
         Size = size;
+        Speed = speed;
+    }
+
+
+    public void MoveUp() {
+        
+    }
+    public void MoveDown() {
+
+    }
+    public void MoveRight() {
+
+    }
+    public void MoveLeft() {
+
     }
 }
