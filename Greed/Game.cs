a54761a14958e66a7c06ec0421ad2b5ccf8b1bf1
JoshZ520 <joshuazob@gmail.com>
2022-11-08 12:24:53 -0700

    added more gems and rock objects

--- Greed/Game.cs
+++ Greed/Game.cs
@@ -18,6 +18,7 @@ public class Game {
         Player player = new Player(Color.BLACK, 5);
         player.Position = player.GetStartPos(ScreenWidth, ScreenHeight);
 
+        
 
         Raylib.InitWindow(ScreenWidth, ScreenHeight, "Greed");
         Raylib.SetTargetFPS(60);
@@ -38,16 +39,28 @@ public class Game {
 
                 switch (whichType) {
                     case 0:
-                        var Rock = new Rocks(Color.BLUE, 10);
+                        var Rock = new Rocks(Color.BLUE, 5);
                         Rock.Position = position;
                         Rock.Velocity = new Vector2(0, 1);
                         Objects.Add(Rock);
                         break;
                     case 1:
+                        var Rock2 = new Rocks(Color.GOLD, 10);
+                        Rock2.Position = position;
+                        Rock2.Velocity = new Vector2(0, 1);
+                        Objects.Add(Rock2);
+                        break;
+                    case 2:
                         var Gem = new Gems(Color.PURPLE, 25);
                         Gem.Position = position;
                         Gem.Velocity = new Vector2(0, 1);
                         Objects.Add(Gem);
+                        break;
+                    case 3:
+                        var Gem2 = new Gems(Color.DARKPURPLE, 30);
+                        Gem2.Position = position;
+                        Gem2.Velocity = new Vector2(0,1);
+                        Objects.Add(Gem2);
                         break;
                     default:
                         break;
